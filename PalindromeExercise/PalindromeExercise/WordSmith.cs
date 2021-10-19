2c2983db712d8376835a29e562dd7ec7b30536b6
tiagogomesgh <tiagogomesfl@outlook.com>
2021-10-19 14:38:35 -0400

    Completed Unit tests and Palidrome Method

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalidrome(string word)
+        {
+            // reverse the word
+            var reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            // compare original with reveresed
+
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            // return true or false
+            
+        }
+
+
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,17 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void Test1(string word, bool expected)
         {
-
+            //arrange
+            var tester = new WordSmith();
+            //act
+            var actual = tester.IsAPalidrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
